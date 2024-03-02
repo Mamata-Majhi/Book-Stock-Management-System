b34b308b5edaa22ead81a93764fdabcf440f360e
Mamata-majhi <mamatach36912@gmail.com>
2024-03-02 09:50:19 +0545

    register page

--- MyProject/user/process_user.php
+++ MyProject/user/process_user.php
@@ -9,18 +9,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gender=$_POST['gender'];
     
 
-    // Inserting data into the user table
-    $sql = "INSERT INTO `user` (`full_name`, `user_name`,`gender`, `password`) 
-            VALUES ('$full_name', '$user_name','$gender', '$password')";
+    // // Inserting data into the user table
+    // $sql = "INSERT INTO `user` (`full_name`, `user_name`,`gender`, `password`) 
+    //         VALUES ('$full_name', '$user_name','$gender', '$password')";
     
+    // // Perform the query
+    // $conn = mysqli_query($conn, $sql);
+
+    // if ($conn) {
+    //     // echo "Data inserted successfully";
+    //     header('location:/Book-Stock-Management-System/MyProject/user/user_login.php');
+    // } else {
+    //     echo "Error: ";
+    // }
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $current_date = date("Y-m-d H:i:s");
+    // Inserting data into the user table
+    $sql = "INSERT INTO user (full_name, user_name,gender, password, created) 
+            VALUES ('$full_name', '$user_name','$gender', '$hashed_password', '$current_date')";
+
     // Perform the query
     $conn = mysqli_query($conn, $sql);
 
     if ($conn) {
         // echo "Data inserted successfully";
-        header('location:/Book-Stock-Management-System/MyProject/dashboard/index.php');
+        echo "<script>
+                        alert(\"User Creation Successful\");
+                        window.location =\"./user_login.php\";
+                </script>";
     } else {
-        echo "Error: ";
+        echo "Error: ";
     }
+
 }
 ?>
